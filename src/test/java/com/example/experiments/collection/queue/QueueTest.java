040ac0b85a775792ef9fce4d54e9ae4c24a14188
Alex Wong <awon6941@uni.sydney.edu.au>
2021-12-02 13:31:33 +0800

    fix queue test pq order

--- src/test/java/com/example/experiments/collection/queue/QueueTest.java
+++ src/test/java/com/example/experiments/collection/queue/QueueTest.java
@@ -18,7 +18,7 @@ public class QueueTest {
     private static PriorityQueue<Employee> employeesPQ;
 
     public static Logger log = LoggerFactory.getLogger(Employee.class);
-    private static List<Employee> finalEmployees;
+    private static List<Employee> finalEmployees, sortByName;
     private static Employee derian, shat, james, jamesw, mo, chloe;
 
     @BeforeAll() // executes once and before BeforeEach
@@ -29,6 +29,7 @@ public class QueueTest {
         jamesw = new Employee("James", "Bond", 19, 0.0);
         mo = new Employee("Mohammed", "Salah", 28, 21000.99);
         chloe = new Employee("Chloe", "Chua", 23, 5000.00);
+        sortByName = new ArrayList<>(List.of(derian, jamesw, james, shat));
         // NOTE: according to natural order (if they implement Comparable) or according to Comparator
         // TODO: initialise employeesPQ with firstlast name in ascending order
         employeesPQ =
@@ -53,15 +54,12 @@ public class QueueTest {
     @Test
     public void TestQueue_InsertionOrder_ShouldFail() {
         // TODO: complete the assert method - assertEquals or assertNotEquals?
-        //  LinkedList
-        assertEquals(employeesLL.toString(), finalEmployees.toString());
+        assert_____(employeesLL.toString(), finalEmployees.toString());
+        assert_____(employeesPQ.toString(), finalEmployees.toString());
 
-        // NOTE: PQ internal structure NOT SORTED, so perform loop with poll()
-        assertFalse(employeesPQ.equals(List.of(derian, jamesw, james, shat)));
-
-        assertNotEquals(getPQOrder(employeesPQ).toString(), finalEmployees.toString());
-        List<Employee> sortedByName = new ArrayList<>(List.of(derian, jamesw, james, shat));
-        assertEquals(getPQOrder(employeesPQ).toString(), sortedByName.toString());
+        assert_____(employeesPQ.toString(), sortByName.toString());
+        // NOTE: PQ internal structure may NOT SORTED, so perform loop with poll()
+        assert_____(getPQOrder(employeesPQ).toString(), sortByName.toString());
     }
 
     public List<Employee> getPQOrder(PriorityQueue<Employee> pq) {
